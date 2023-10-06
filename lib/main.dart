54edea0436539def90e92bd16b31e2afddddb6ef
Shakibul Alam <78785427+ShakibulDeveloper@users.noreply.github.com>
2023-10-06 23:53:55 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,17 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     home: const HomeScreenState(),
-     title: "App",
-     theme: ThemeData(
-       elevatedButtonTheme: ElevatedButtonThemeData(
-         style: ElevatedButton.styleFrom(
-           backgroundColor: const Color(0xFF9e9e9e),
-         ),
-       ),
-     ),
-   );
+    return MaterialApp(
+      home: const HomeScreenState(),
+      title: "App",
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF9e9e9e),
+          ),
+        ),
+      ),
+    );
   }
 
 }
@@ -37,87 +37,93 @@ class HomeScreenUI extends State<HomeScreenState>{
 
   @override
   Widget build(BuildContext context) {
+
+    //======== SNACK_BAR =========
     void appSnackBar(context, String message){
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text(message))
       );
     }
-    
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Size Selector"),
       ),
-      body: ButtonBar(
-        alignment: MainAxisAlignment.start,
-        children: [
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "S";
-                appSnackBar(context, "S");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "S" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("S")
-          ),
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "M";
-                appSnackBar(context, "M");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "M" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("M")
-          ),
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "L";
-                appSnackBar(context, "L");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "L" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("L")
-          ),
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "XL";
-                appSnackBar(context, "XL");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "XL" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("XL")
-          ),
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "XXL";
-                appSnackBar(context, "XXL");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "XXL" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("XXL")
-          ),
-          ElevatedButton(
-              onPressed: (){
-                activeOnClick = "XXXL";
-                appSnackBar(context, "XXXL");
-                setState(() {});
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(activeOnClick == "XXXL" ? 0xFFff9800 : 0xFF9e9e9e),
-              ),
-              child: const Text("XXXL")
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Wrap(
+          spacing: 5,
+          runSpacing: 6,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "S";
+                  appSnackBar(context, "S");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "S" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("S")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "M";
+                  appSnackBar(context, "M");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "M" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("M")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "L";
+                  appSnackBar(context, "L");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "L" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("L")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "XL";
+                  appSnackBar(context, "XL");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "XL" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("XL")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "XXL";
+                  appSnackBar(context, "XXL");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "XXL" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("XXL")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  activeOnClick = "XXXL";
+                  appSnackBar(context, "XXXL");
+                  setState(() {});
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(activeOnClick == "XXXL" ? 0xFFff9800 : 0xFF9e9e9e),
+                ),
+                child: const Text("XXXL")
+            ),
+          ],
+        ),
       ),
     );
   }
